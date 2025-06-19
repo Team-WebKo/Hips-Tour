0d15046ae89aeff9d5d2921bab172cb257892589
Baskervir <persona903@gmail.com>
2025-06-19 22:17:27 +0900

    [COMMON] Place.java - @Setter 추가
     추가적인 기능이 없는 부분에 대한 @Setter추가로 가독성 향상

--- common/src/main/java/com/project/hiptour/common/place/Place.java
+++ common/src/main/java/com/project/hiptour/common/place/Place.java
@@ -6,11 +6,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 public class Place {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,38 +34,5 @@ public class Place {
         this.address2 = address2;
         this.longitude = longitude;
         this.latitude = latitude;
-    }
-
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setPlaceName(String placeName) {
-        this.placeName = placeName;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public void setAddress1(String address1) {
-        this.address1 = address1;
-    }
-
-    public void setAddress2(String address2) {
-        this.address2 = address2;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
     }
 }
