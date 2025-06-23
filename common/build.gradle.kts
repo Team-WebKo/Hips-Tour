200d5ece78df707a63ab0b69bff6d5160f4671ec
Ainchel <ckd3935@gmail.com>
2025-06-23 23:57:32 +0900

    build : OAuth2 카카오 로그인 구현을 위한 의존 추가
    application.yaml 파일 및 build.gradle.kts 파일에 의존 및 설정 추가

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("com.h2database:h2")
