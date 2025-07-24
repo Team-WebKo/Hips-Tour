4fc8bf86b11e46382be9d123a5a5b0e36b399f3a
Baskervir <persona903@gmail.com>
2025-07-24 12:10:03 +0900

    [COMMON] 리뷰 작성 기능 추가
     임의의 User 추가
     리뷰 작성 기능 추가하였습니다.

--- common/src/main/java/com/project/hiptour/common/reviews/controller/ReviewController.java
+++ common/src/main/java/com/project/hiptour/common/reviews/controller/ReviewController.java
@@ -3,9 +3,12 @@ package com.project.hiptour.common.reviews.controller;
 import com.project.hiptour.common.reviews.dto.CreateReviewRequestDto;
 import com.project.hiptour.common.reviews.repository.PlaceRepository;
 import com.project.hiptour.common.reviews.service.CreateReviewService;
+import com.project.hiptour.common.users.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -16,9 +19,17 @@ public class ReviewController {
 
     @PostMapping("/places/{placeId}/reviews")
     public ResponseEntity<?> createReview(
-            @PathVariable Long placeId,
             @RequestBody CreateReviewRequestDto reviewRequestDto
+            // 유저 인증 정보 필요
             ) {
-        Long reviewId = createReviewService.create(placeId, reviewRequestDto, );
+        User user = new User(); /**유저 정보 및 인증 에 대한 추가 이후 방향 결정 필요**/
+        Long reviewId = createReviewService.create(reviewRequestDto, user);
+        return ResponseEntity.ok(Map.of("reviewId", reviewId));
+/**
+ * 현재 User 객체를 직접 생성하도록 되어있습니다.
+ * 추후 유저 및 로그인 기능 추가 이후 작업 필요합니다.
+ *
+ *
+ * **/
     }
 }
--- common/src/main/java/com/project/hiptour/common/reviews/service/CreateReviewService.java
+++ common/src/main/java/com/project/hiptour/common/reviews/service/CreateReviewService.java
@@ -5,6 +5,7 @@ import com.project.hiptour.common.reviews.dto.CreateReviewRequestDto;
 import com.project.hiptour.common.reviews.entity.Review;
 import com.project.hiptour.common.reviews.repository.PlaceRepository;
 import com.project.hiptour.common.reviews.repository.ReviewRepository;
+import com.project.hiptour.common.users.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,7 @@ public class CreateReviewService {
     private final ReviewRepository reviewRepository;
     private final PlaceRepository placeRepository;
 
-    public Long create(CreateReviewRequestDto requestDto, Long userId, String nickname) {
+    public Long create(CreateReviewRequestDto requestDto, User user) {
         Place place = placeRepository.findById(requestDto.getPlaceId()).orElseThrow(() -> new IllegalArgumentException("해당 장소는 존재하지 않습니다."));
 
         Review review = Review.builder()
@@ -22,8 +23,7 @@ public class CreateReviewService {
                 .content(requestDto.getContent())
                 .isLove(requestDto.getIsLove())
                 .imageUrls(requestDto.getImageUrls())
-                .userId(userId)
-                .nickname(nickname)
+                .user(user)
                 .build();
 
         reviewRepository.save(review);
