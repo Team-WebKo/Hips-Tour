fe0981b4c2392cc88ff2e5a153dc0d6590a863ef
Eunoos <dmsdn18@gmail.com>
2025-07-24 15:50:50 +0900

    [COMMON][REVIEW] 여행지 리뷰 조회 기능 추가 #30
     컨트롤러 코드누락

--- common/src/main/java/com/project/hiptour/common/reviews/controller/ReviewController.java
+++ common/src/main/java/com/project/hiptour/common/reviews/controller/ReviewController.java
@@ -1,14 +1,33 @@
 package com.project.hiptour.common.reviews.controller;
 
 import com.project.hiptour.common.reviews.service.CreateReviewService;
+import com.project.hiptour.common.reviews.dto.ReviewListResponseDto;
+import com.project.hiptour.common.reviews.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ReviewController {
-    private final CreateReviewService createReviewService;
 
+    private final CreateReviewService createReviewService;
+    private final ReviewService reviewService;
+
+
+
+    @Operation(summary = "리뷰 목록 조회", description = "장소 ID(placeId)에 해당하는 리뷰 offset, limit 나눠 조회")
+    @GetMapping("/place/{placeId}")
+    public List<ReviewListResponseDto> getReviewsByPlaceId(
+            @Parameter(description = "장소 ID") @PathVariable Long placeId,
+            @Parameter(description = "가져올 시작 위치") @RequestParam int offset,
+            @Parameter(description = "가져올 개수") @RequestParam int limit
+    ) {
+        return reviewService.getReviewsByPlaceId(placeId, offset, limit);
+    }
 }
