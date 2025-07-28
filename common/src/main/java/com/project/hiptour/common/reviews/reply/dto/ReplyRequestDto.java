5529e2d970cd566c3b03cf2a1870dd5db6d4dd48
Baskervir <persona903@gmail.com>
2025-07-28 18:47:45 +0900

    [REVIEW][REPLY] 여행지 리뷰에 대한 댓글 기능 controller 추가
     id중복을 방지 하며, reviewId를 DTO에 반영 하는 기능 추가 및 컨트롤러 조합

--- common/src/main/java/com/project/hiptour/common/reviews/reply/dto/ReplyRequestDto.java
+++ common/src/main/java/com/project/hiptour/common/reviews/reply/dto/ReplyRequestDto.java
@@ -1,10 +1,12 @@
 package com.project.hiptour.common.reviews.reply.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReplyRequestDto {
     private Long reviewId;
     private String content;
