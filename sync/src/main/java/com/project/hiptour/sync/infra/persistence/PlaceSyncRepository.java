fb3097509ef5a88b4a98eb9f761a5a534f10ab2a
Baskervir <persona903@gmail.com>
2025-08-07 17:43:06 +0900

    [SYNC] PlaceSyncRepository 수정
     필수 애너테이션 추가

--- sync/src/main/java/com/project/hiptour/sync/infra/persistence/PlaceSyncRepository.java
+++ sync/src/main/java/com/project/hiptour/sync/infra/persistence/PlaceSyncRepository.java
@@ -2,7 +2,8 @@ package com.project.hiptour.sync.infra.persistence;
 
 import com.project.hiptour.common.place.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaceSyncRepository extends JpaRepository<Place, Long> {
-
 }
