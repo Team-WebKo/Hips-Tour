e2a7cd6dd65b59de3980c13334ff62ba84b48ca9
Baskervir <persona903@gmail.com>
2025-08-07 18:44:02 +0900

    [COMMON] 컴포넌트 스캔 범위 추가
     서브 모듈인 동기화 API를 사용하기 위한 컴포넌트 스캔 범위 추가

--- common/src/main/java/com/project/hiptour/common/CommonApplication.java
+++ common/src/main/java/com/project/hiptour/common/CommonApplication.java
@@ -3,7 +3,10 @@ package com.project.hiptour.common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.project.hiptour.common",
+		"com.project.hiptour.sync"
+})
 public class CommonApplication {
 
 	public static void main(String[] args) {
