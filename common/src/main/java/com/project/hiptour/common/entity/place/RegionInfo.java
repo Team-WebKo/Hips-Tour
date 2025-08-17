b6c6dda1475e4c848afa17ac9ebe7afd59cb97f1
hyunil-cho <jackandjills@naver.com>
2025-08-17 15:24:27 +0900

    [ERD-Mapping] 리전 관련하여 키워드와 겹칠 수 있음으로, 이름 변경

--- common/src/main/java/com/project/hiptour/common/entity/place/RegionInfo.java
+++ common/src/main/java/com/project/hiptour/common/entity/place/RegionInfo.java
@@ -1,18 +1,14 @@
 package com.project.hiptour.common.entity.place;
 
-import com.project.hiptour.common.entity.BaseTimeEntity;
 import com.project.hiptour.common.entity.BaseUpdateEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.ToString;
 
 @Entity
 @Getter
 @ToString
-public class Region extends BaseUpdateEntity {
+public class RegionInfo extends BaseUpdateEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer regionId;
