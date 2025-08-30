a9dc3f4eb6f5a2f38e35ee17ebdfea2d01954162
hyunil <jackandjills@naver.com>
2025-08-30 23:22:38 +0900

    jwt 처리를 위한 라이브러리 추가

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,6 +11,6 @@ dependencies {
 	testImplementation("com.h2database:h2")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-
+// https://mvnrepository.com/artifact/com.auth0/java-jwt
+    implementation("com.auth0:java-jwt:4.5.0")
 }
