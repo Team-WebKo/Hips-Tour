e2cfcad34bb1335a37772e0eb4a751ea7a466479
hyunil <jackandjills@naver.com>
2025-09-06 17:26:44 +0900

    불필요한 코드 삭제

--- common/src/main/java/com/project/hiptour/common/usercase/services/token/TokenProvider.java
+++
@@ -1,8 +0,0 @@
-package com.project.hiptour.common.usercase.services.token;
-
-import com.project.hiptour.common.entity.users.UserInfo;
-import com.project.hiptour.common.security.UserIdentity;
-
-public interface TokenProvider {
-    String generateToken(UserInfo user);
-}
