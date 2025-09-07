9cf783da8ef28aaca7fe5ae9b3f47335a402f00b
Baskervir <persona903@gmail.com>
2025-09-08 00:26:45 +0900

    [COMMON] SecurityConfig 추가
     기본 보안 정책 수립
     Review, Logout 기능 활용을 위한 정책

---
+++ common/src/main/java/com/project/hiptour/common/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.project.hiptour.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic(basic -> basic.disable())
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests.requestMatchers(HttpMethod.POST, "/logout", "/reviews").authenticated()
+                        .anyRequest().permitAll());
+
+        // TODO: JWT 필터 추가 예정
+
+        return httpSecurity.build();
+    }
+}
