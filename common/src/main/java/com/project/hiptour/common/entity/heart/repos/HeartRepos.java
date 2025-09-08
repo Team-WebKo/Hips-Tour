1f798d878da229a901b8292bbf64994bf2a0f475
hyunil <jackandjills@naver.com>
2025-09-08 23:30:28 +0900

    찜하기 레포 추가

---
+++ common/src/main/java/com/project/hiptour/common/entity/heart/repos/HeartRepos.java
@@ -0,0 +1,9 @@
+package com.project.hiptour.common.entity.heart.repos;
+
+import com.project.hiptour.common.entity.heart.Heart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HeartRepos extends JpaRepository<Heart,Long> {
+}
