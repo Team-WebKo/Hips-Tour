adf63bfc5b3028723f25ad1b2fc9061860605c01
hyunil <jackandjills@naver.com>
2025-09-15 22:16:34 +0900

    usecase 응답값에 따라 컨트롤러 응답값 변경

--- common/src/main/java/com/project/hiptour/common/web/auth/logout/LogoutController.java
+++ common/src/main/java/com/project/hiptour/common/web/auth/logout/LogoutController.java
@@ -1,5 +1,6 @@
 package com.project.hiptour.common.web.auth.logout;
 
+import com.project.hiptour.common.usercase.logout.LogoutResponse;
 import com.project.hiptour.common.usercase.logout.UserLogoutUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +19,12 @@ public class LogoutController {
 
     @PostMapping
     public ResponseEntity<Map<String, String>> logout(@RequestHeader("Authorization") String authorizationHeader) {
-        userLogoutUseCase.logout(authorizationHeader);
+        LogoutResponse logoutResponse = userLogoutUseCase.logout(authorizationHeader);
 
-        return ResponseEntity.ok(Map.of("message", "로그아웃에 성공했습니다."));
+        if(logoutResponse.isSuccess()){
+            return ResponseEntity.ok(Map.of("message", "로그아웃에 성공했습니다."));
+        }
+
+        return ResponseEntity.badRequest().body(Map.of("message", logoutResponse.getMessage()));
     }
 }
