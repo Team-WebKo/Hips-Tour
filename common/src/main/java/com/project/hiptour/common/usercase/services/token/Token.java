783f36b76658bccae2faf3bff59b1df0205f0028
hyunil <jackandjills@naver.com>
2025-09-16 21:05:37 +0900

    Token 객체 내에, 만료 시간을 가져오는 메서드 추가

--- common/src/main/java/com/project/hiptour/common/usercase/services/token/Token.java
+++ common/src/main/java/com/project/hiptour/common/usercase/services/token/Token.java
@@ -1,10 +1,16 @@
 package com.project.hiptour.common.usercase.services.token;
 
+import com.auth0.jwt.JWT;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Base64;
+import java.util.Date;
 
 @AllArgsConstructor
 @Getter
@@ -15,11 +21,17 @@ public class Token {
     private final String payload;
     private final String signature;
 
-    public Token(String token, LocalDateTime createdTime, LocalDateTime expireTime) {
+    public Token(String token) {
         this.token = token;
         this.header = token.substring(0, token.indexOf("."));
         this.payload = token.substring(token.indexOf(".")+1, token.lastIndexOf("."));
         this.signature = token.substring(token.lastIndexOf(".")+1);
+    }
+
+    public LocalDateTime getExpireDate(){
+        Date expiresAt = JWT.decode(this.token).getExpiresAt();
+        Instant instant = expiresAt.toInstant();
+        return instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 
     public String getPayLoad(){
