06805de61ad7c3c80dab3a1f816fb00254a4cd83
Baskervir <persona903@gmail.com>
2025-09-16 23:13:39 +0900

    [COMMON][REVIEW] 찜 순으로 여행지 추천 로직 제거
     TelNumber 에 생성자용 어노테이션 추가

--- common/src/main/java/com/project/hiptour/common/place/serviceImpl/PlaceServiceImpl.java
+++ common/src/main/java/com/project/hiptour/common/place/serviceImpl/PlaceServiceImpl.java
@@ -33,34 +33,6 @@ public class PlaceServiceImpl implements PlaceService {
 
     @Override
     public Page<PlaceDto> findRecommendedPlaces(Pageable pageable) {
-        Page<Place> heartedPlacesPage = placeRepository.findPlacesOrderByHeartCount(pageable);
-
-        if (!heartedPlacesPage.isEmpty()) {
-            if (heartedPlacesPage.getContent().size() < pageable.getPageSize()) {
-                List<Place> heartedPlacesOrSortedPlaces = new ArrayList<>(heartedPlacesPage.getContent());
-                int remainingLimit = pageable.getPageSize() - heartedPlacesOrSortedPlaces.size();
-
-                if (remainingLimit > 0) {
-                    List<Integer> excludedIds = heartedPlacesOrSortedPlaces.stream()
-                            .map(Place::getPlaceId)
-                            .collect(Collectors.toList());
-
-                    Pageable remainingPageable = PageRequest.of(0, remainingLimit);
-                    Page<Place> recentPlacesPage = placeRepository.findByPlaceIdNotInOrderByCreatedAtDesc(excludedIds, remainingPageable);
-
-                    heartedPlacesOrSortedPlaces.addAll(recentPlacesPage.getContent());
-                }
-
-                return new PageImpl<>(
-                        heartedPlacesOrSortedPlaces.stream().map(PlaceDto::from).collect(Collectors.toList()),
-                        pageable,
-                        heartedPlacesPage.getTotalElements()
-                );
-            }
-
-            return heartedPlacesPage.map(PlaceDto::from);
-        }
-
         return placeRepository.findAllByOrderByCreatedAtDesc(pageable).map(PlaceDto::from);
     }
 }
--- common/src/main/java/com/project/hiptour/common/reviews/controller/ReviewController.java
+++ common/src/main/java/com/project/hiptour/common/reviews/controller/ReviewController.java
@@ -28,7 +28,7 @@ public class ReviewController {
 
     @Operation(summary = "리뷰 작성", description = "특정 장소 url을 파라미터로 받아 사용")
     @PostMapping("/places/{placeId}/reviews")
-    public ResponseEntity<?> createReview(@PathVariable Integer placeId, @RequestBody CreateReviewRequestDto requestDto) {
+    public ResponseEntity<?> createReview(@PathVariable("placeId") Integer placeId, @RequestBody CreateReviewRequestDto requestDto) {
         UserInfo userInfo = userRepos.findById(requestDto.getUserId())
                 .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다: " + requestDto.getUserId()));
 
@@ -39,7 +39,7 @@ public class ReviewController {
     @Operation(summary = "리뷰 목록 조회", description = "장소에 대한 모든 리뷰를 조회합니다.")
     @GetMapping("/places/{placeId}/reviews")
     public Page<ReviewListResponseDto> getReviewByPlace(
-            @PathVariable Integer placeId,
+            @PathVariable("placeId") Integer placeId,
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 
         return reviewService.getReviewsByPlace(placeId, pageable);
@@ -48,7 +48,7 @@ public class ReviewController {
     @Operation(summary = "내가 작성한 리뷰 목록 조회", description = "내가 작성한 리뷰 목록만 조회합니다. 수정 및 삭제에 활용")
     @GetMapping("reviews/my")
     public Page<MyReviewResponseDto> getMyReviews(
-            @RequestParam Long userId,
+            @RequestParam("userId") Long userId,
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ) {
         UserInfo userInfo = userRepos.findById(userId)
@@ -60,7 +60,7 @@ public class ReviewController {
     @Operation(summary = "리뷰 수정", description = "리뷰 수정을 위해서는 우선 내가 작성한 리뷰 목록을 조회해야 합니다.")
     @PatchMapping("/reviews/{reviewId}")
     public ResponseEntity<Void> updateReview(
-            @PathVariable Long reviewId,
+            @PathVariable("reviewId") Long reviewId,
             @RequestBody UpdateReviewRequestDto requestDto
             ) {
         UserInfo userInfo = userRepos.findById(requestDto.getUserId())
@@ -73,8 +73,8 @@ public class ReviewController {
     @Operation(summary = "리뷰 삭제", description = "리뷰 삭제를 위해선 내가 작성한 리뷰 목록을 조회해야 합니다.")
     @DeleteMapping("/reviews/{reviewId}")
     public ResponseEntity<Void> deleteReview(
-            @PathVariable Long reviewId,
-            @RequestParam Long userId
+            @PathVariable("reviewId") Long reviewId,
+            @RequestParam("userId") Long userId
     ) {
         UserInfo userInfo = userRepos.findById(userId).orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다: " + userId));
 
