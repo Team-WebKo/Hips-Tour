83113ef3eb8c85b9cf51d832f88c6b9f91cfa514
hyunil <jackandjills@naver.com>
2025-09-16 20:56:13 +0900

    Token 객체 내에 불필요한 시간 값 삭제

--- common/src/main/java/com/project/hiptour/common/usercase/services/token/Token.java
+++ common/src/main/java/com/project/hiptour/common/usercase/services/token/Token.java
@@ -14,13 +14,9 @@ public class Token {
     private final String header;
     private final String payload;
     private final String signature;
-    private final LocalDateTime createdTime;
-    private final LocalDateTime expireTime;
 
     public Token(String token, LocalDateTime createdTime, LocalDateTime expireTime) {
         this.token = token;
-        this.createdTime = createdTime;
-        this.expireTime = expireTime;
         this.header = token.substring(0, token.indexOf("."));
         this.payload = token.substring(token.indexOf(".")+1, token.lastIndexOf("."));
         this.signature = token.substring(token.lastIndexOf(".")+1);
