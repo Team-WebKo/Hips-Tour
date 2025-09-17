54ba5c30d52764bb82803ea89600bdea0fba1bb0
Baskervir <persona903@gmail.com>
2025-09-18 00:48:02 +0900

    [COMMON][EXCEPTION] 예외 처리에 주석 추가
     예외 처리 상위 경로 및 예외의 역할을 명시합니다.

--- common/src/main/java/com/project/hiptour/common/exception/InvalidAccessException.java
+++ common/src/main/java/com/project/hiptour/common/exception/InvalidAccessException.java
@@ -2,6 +2,9 @@ package com.project.hiptour.common.exception;
 
 /**
  * 리소스에 대한 접근 권한이 없을 때 발생하는 예외들의 부모 클래스 (HTTP 403 Forbidden)
+ *
+ * 이 예외를 상속하는 모든 자식 예외는
+ * {@link com.project.hiptour.common.exception.GlobalExceptionHandler}에서 일괄 처리됩니다.
  */
 public abstract class InvalidAccessException extends RuntimeException {
     public InvalidAccessException(String message) {
--- common/src/main/java/com/project/hiptour/common/exception/UserNotFoundException.java
+++ common/src/main/java/com/project/hiptour/common/exception/UserNotFoundException.java
@@ -1,11 +1,15 @@
 package com.project.hiptour.common.exception;
 
+/**
+ * 요청한 유저를 찾을 수 없을 때 발생하는 예외입니다.
+ * 이 예외는 {@link GlobalExceptionHandler}에서 처리되어 HTTP 404 응답을 반환합니다.
+ */
 public class UserNotFoundException extends NotFoundException {
-  public UserNotFoundException() {
-    super("해당 유저를 찾을 수 없습니다.");
-  }
+    public UserNotFoundException() {
+        super("해당 유저를 찾을 수 없습니다.");
+    }
 
-  public UserNotFoundException(String message) {
-    super(message);
-  }
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
